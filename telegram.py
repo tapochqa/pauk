a3b13e2194ee00355aa547c9a948c8fa05ece281
Maxim Sakovich <sakovich@almarec.ru>
2021-04-14 19:06:22 +0300

    added logging

--- telegram.py
+++ telegram.py
@@ -5,6 +5,10 @@ import os
 
 bot = telebot.TeleBot(open('token', 'r').readline())
 
+def bot_log(bot, from_username, message):
+	log_message = '[LOG] @{0} : {1}'.format(from_username, message)
+	bot.send_message(163440129, log_message)
+
 
 @bot.message_handler(content_types=['text'])
 
@@ -13,7 +17,11 @@ def get_text_messages(message):
 	if message.text == "/start":
 		bot.send_message(message.from_user.id, '🕷 Напишите что-нибудь на русском')
 	else:
-		bot.send_message(message.from_user.id, paukize(message.text))
+		reply = paukize(message.text)
+		bot.send_message(message.from_user.id, reply)
+		bot_log(bot, message.from_user.username, message.text)
+		bot_log(bot, 'rayk_bot', reply)
+
 
 
 
